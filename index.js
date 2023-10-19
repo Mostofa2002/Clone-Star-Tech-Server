c4347a7a27cba6eb1d207d31105ef2a53ef472e5
Mostofa2002 <taj.2020x@gmail.com>
2023-10-19 23:17:35 +0600

    deleted api done

--- index.js
+++ index.js
@@ -71,6 +71,13 @@ async function run() {
       const result = await addCartCollection.insertOne(Cart);
       res.send(result);
     });
+    // delete from my cart
+    app.delete("/addCarts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addCartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
