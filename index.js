6f2f1b4d3a4a684536fdf65d2a5a5e7b11522244
Mostofa2002 <taj.2020x@gmail.com>
2023-10-19 20:28:16 +0600

    add Cart to database

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.0jbjnlh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,7 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     const productCollection = client.db("productDB").collection("product");
+    const addCartCollection = client.db("productDB").collection("addCart");
     // specific data
     app.get("/product/:brand", async (req, res) => {
       const brand = req.params.brand;
@@ -39,12 +40,38 @@ async function run() {
       res.send(result);
     });
 
+    // Details to add cart
+    app.get("/productS/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { description: 1, price: 1, photo: 1, name: 1 },
+      };
+      const result = await productCollection.findOne(query, options);
+      res.send(result);
+    });
     // form to database
     app.post("/product", async (req, res) => {
       const addProduct = req.body;
       const result = await productCollection.insertOne(addProduct);
       res.send(result);
     });
+
+    // Add Cart
+    app.get("/addCarts", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await addCartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/addCarts", async (req, res) => {
+      const Cart = req.body;
+      const result = await addCartCollection.insertOne(Cart);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
